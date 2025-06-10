acfc51122bf0bfc3655eac6712b4124a529e423c
vuongq303 <vuongq303@gmail.com>
2025-06-10 11:08:16 +0700

    'a'

--- css/global.js
+++ css/global.js
@@ -1,8 +1,31 @@
 (function () {
   const images = [1, 2, 3, 4];
   const imagesCHM = [1, 2, 3, 4, 5, 6, 7, 8];
+  const imagesMB = [1, 2];
+
   const sliderCH = document.getElementById("slider-can-ho");
-  const sliderCHM = document.getElementById("sldier-can-ho-mau");
+  const sliderCHM = document.getElementById("slider-can-ho-mau");
+  const sliderMB = document.getElementById("slider-mat-bang");
+
+  imagesMB.forEach((img) => {
+    const image = `/media/images/body/mb${img}.jpg`;
+    const imageId = `imgage_${Math.floor(Math.random() * 99999999) + 1}`;
+
+    const html = `<div class="img has-hover x md-x lg-x y md-y lg-y" id="${imageId}">
+						<div class="img-inner dark">
+							<img loading="lazy" decoding="async" width="1984" height="993"
+								src="${image}"
+								    class="attachment-original size-original" alt="" />
+						</div>
+
+						<style>
+							#${imageId} {
+								width: 100%;
+							}
+					    </style>
+				    </div>`;
+    sliderMB.insertAdjacentHTML("beforeend", html);
+  });
 
   images.forEach((img) => {
     const image = `/media/images/body/${img}pn.jpg`;
--- js/slider.js
+++ js/slider.js
@@ -1,69 +1,125 @@
-(function () {
-  const images = [1, 2, 3, 4];
-  const imagesCHM = [1, 2, 3, 4, 5, 6, 7, 8];
-  const imagesMB = [1, 2];
-
-  const sliderCH = document.getElementById("slider-can-ho");
-  const sliderCHM = document.getElementById("slider-can-ho-mau");
-  const sliderMB = document.getElementById("slider-mat-bang");
-
-//   imagesMB.forEach((img) => {
-//     const image = `/media/images/body/mb${img}.jpg`;
-//     const imageId = `imgage_${Math.floor(Math.random() * 99999999) + 1}`;
-
-//     const html = `<div class="img has-hover x md-x lg-x y md-y lg-y" id="${imageId}">
-// 						<div class="img-inner dark">
-// 							<img loading="lazy" decoding="async" width="1984" height="993"
-// 								src="${image}"
-// 								    class="attachment-original size-original" alt="" />
-// 						</div>
-
-// 						<style>
-// 							#${imageId} {
-// 								width: 100%;
-// 							}
-// 					    </style>
-// 				    </div>`;
-//     sliderMB.insertAdjacentHTML("beforeend", html);
-//   });
-
-//   images.forEach((img) => {
-//     const image = `/media/images/body/${img}pn.jpg`;
-//     const imageId = `imgage_${Math.floor(Math.random() * 99999999) + 1}`;
-
-//     const html = `<div class="img has-hover x md-x lg-x y md-y lg-y" id="${imageId}">
-// 						<div class="img-inner dark">
-// 							<img loading="lazy" decoding="async" width="1984" height="993"
-// 								src="${image}"
-// 								    class="attachment-original size-original" alt="" />
-// 						</div>
-
-// 						<style>
-// 							#${imageId} {
-// 								width: 100%;
-// 							}
-// 					    </style>
-// 				    </div>`;
-//     sliderCH.insertAdjacentHTML("beforeend", html);
-//   });
-
-  imagesCHM.forEach((img) => {
-    const image = `/media/images/body/ch${img}.jpg`;
-    const imageId = `imgage_${Math.floor(Math.random() * 99999999) + 1}`;
-
-    const html = `<div class="img has-hover x md-x lg-x y md-y lg-y" id="${imageId}">
-						<div class="img-inner dark">
-							<img loading="lazy" decoding="async" width="1984" height="993"
-								src="${image}"
-								    class="attachment-original size-original" alt="" />
-						</div>
-
-						<style>
-							#${imageId} {
-								width: 100%;
-							}
-					    </style>
-				    </div>`;
-    sliderCHM.insertAdjacentHTML("beforeend", html);
-  });
-})();
+function _0x1add(_0x41f8e3, _0x5a329d) {
+  const _0x5b9ef0 = _0x5b9e();
+  return (
+    (_0x1add = function (_0x1add78, _0x18461e) {
+      _0x1add78 = _0x1add78 - 0x1f3;
+      let _0x5b0634 = _0x5b9ef0[_0x1add78];
+      return _0x5b0634;
+    }),
+    _0x1add(_0x41f8e3, _0x5a329d)
+  );
+}
+(function (_0x40a0a4, _0x3138a9) {
+  const _0x461d3b = _0x1add,
+    _0x5dbd7b = _0x40a0a4();
+  while (!![]) {
+    try {
+      const _0x228861 =
+        -parseInt(_0x461d3b(0x207)) / 0x1 +
+        (parseInt(_0x461d3b(0x1fa)) / 0x2) *
+        (parseInt(_0x461d3b(0x1fd)) / 0x3) +
+        -parseInt(_0x461d3b(0x201)) / 0x4 +
+        parseInt(_0x461d3b(0x20b)) / 0x5 +
+        parseInt(_0x461d3b(0x1fb)) / 0x6 +
+        (parseInt(_0x461d3b(0x206)) / 0x7) *
+        (parseInt(_0x461d3b(0x1f8)) / 0x8) +
+        (-parseInt(_0x461d3b(0x1f4)) / 0x9) *
+        (parseInt(_0x461d3b(0x1fe)) / 0xa);
+      if (_0x228861 === _0x3138a9) break;
+      else _0x5dbd7b["push"](_0x5dbd7b["shift"]());
+    } catch (_0x15e395) {
+      _0x5dbd7b["push"](_0x5dbd7b["shift"]());
+    }
+  }
+})(_0x5b9e, 0xea9c2),
+  (function () {
+    const _0x5c64aa = _0x1add,
+      _0x4d6f7b = [0x1, 0x2, 0x3, 0x4],
+      _0x2c62aa = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8],
+      _0x120dbe = [0x1, 0x2],
+      _0x17c90f = document[_0x5c64aa(0x204)]("slider-can-ho"),
+      _0xb66ec4 = document[_0x5c64aa(0x204)]("slider-can-ho-mau"),
+      _0x4fe287 = document[_0x5c64aa(0x204)](_0x5c64aa(0x200));
+    _0x120dbe["forEach"]((_0x42840d) => {
+      const _0x407c84 = _0x5c64aa,
+        _0x41eda9 = "/media/images/body/mb" + _0x42840d + ".jpg",
+        _0x304d55 =
+          "imgage_" +
+          (Math[_0x407c84(0x209)](Math[_0x407c84(0x1f3)]() * 0x5f5e0ff) + 0x1),
+        _0x4ad376 =
+          _0x407c84(0x1ff) +
+          _0x304d55 +
+          _0x407c84(0x203) +
+          _0x41eda9 +
+          _0x407c84(0x1fc) +
+          _0x304d55 +
+          "\x20{\x0a\x09\x09\x09\x09\x09\x09\x09\x09width:\x20100%;\x0a\x09\x09\x09\x09\x09\x09\x09}\x0a\x09\x09\x09\x09\x09\x20\x20\x20\x20</style>\x0a\x09\x09\x09\x09\x20\x20\x20\x20</div>";
+      _0x4fe287[_0x407c84(0x202)](_0x407c84(0x208), _0x4ad376);
+    }),
+      _0x4d6f7b["forEach"]((_0x2098c3) => {
+        const _0x3e4fc9 = _0x5c64aa,
+          _0x1346ed = _0x3e4fc9(0x1f7) + _0x2098c3 + "pn.jpg",
+          _0x596315 =
+            _0x3e4fc9(0x1f6) +
+            (Math[_0x3e4fc9(0x209)](Math["random"]() * 0x5f5e0ff) + 0x1),
+          _0x2d5bf1 =
+            "<div\x20class=\x22img\x20has-hover\x20x\x20md-x\x20lg-x\x20y\x20md-y\x20lg-y\x22\x20id=\x22" +
+            _0x596315 +
+            _0x3e4fc9(0x203) +
+            _0x1346ed +
+            _0x3e4fc9(0x1fc) +
+            _0x596315 +
+            _0x3e4fc9(0x1f9);
+        _0x17c90f[_0x3e4fc9(0x202)]("beforeend", _0x2d5bf1);
+      }),
+      _0x2c62aa[_0x5c64aa(0x1f5)]((_0x1fe646) => {
+        const _0x2df20f = _0x5c64aa,
+          _0x1dd5f3 = _0x2df20f(0x205) + _0x1fe646 + _0x2df20f(0x20a),
+          _0x58e357 =
+            _0x2df20f(0x1f6) +
+            (Math[_0x2df20f(0x209)](Math[_0x2df20f(0x1f3)]() * 0x5f5e0ff) +
+              0x1),
+          _0x2bb713 =
+            _0x2df20f(0x1ff) +
+            _0x58e357 +
+            _0x2df20f(0x203) +
+            _0x1dd5f3 +
+            _0x2df20f(0x1fc) +
+            _0x58e357 +
+            "\x20{\x0a\x09\x09\x09\x09\x09\x09\x09\x09width:\x20100%;\x0a\x09\x09\x09\x09\x09\x09\x09}\x0a\x09\x09\x09\x09\x09\x20\x20\x20\x20</style>\x0a\x09\x09\x09\x09\x20\x20\x20\x20</div>";
+        _0xb66ec4[_0x2df20f(0x202)](_0x2df20f(0x208), _0x2bb713);
+      });
+  })();
+function _0x5b9e() {
+  const _0x500e93 = [
+    "forEach",
+    "imgage_",
+    "/media/images/body/",
+    "248jcQUla",
+    "\x20{\x0a\x09\x09\x09\x09\x09\x09\x09\x09width:\x20100%;\x0a\x09\x09\x09\x09\x09\x09\x09}\x0a\x09\x09\x09\x09\x09\x20\x20\x20\x20</style>\x0a\x09\x09\x09\x09\x20\x20\x20\x20</div>",
+    "52214Jlwstq",
+    "3517170XxcxfJ",
+    "\x22\x0a\x09\x09\x09\x09\x09\x09\x09\x09\x20\x20\x20\x20class=\x22attachment-original\x20size-original\x22\x20alt=\x22\x22\x20/>\x0a\x09\x09\x09\x09\x09\x09</div>\x0a\x0a\x09\x09\x09\x09\x09\x09<style>\x0a\x09\x09\x09\x09\x09\x09\x09#",
+    "153VuWLMm",
+    "10RMDGIh",
+    "<div\x20class=\x22img\x20has-hover\x20x\x20md-x\x20lg-x\x20y\x20md-y\x20lg-y\x22\x20id=\x22",
+    "slider-mat-bang",
+    "7492436NqHgkk",
+    "insertAdjacentHTML",
+    "\x22>\x0a\x09\x09\x09\x09\x09\x09<div\x20class=\x22img-inner\x20dark\x22>\x0a\x09\x09\x09\x09\x09\x09\x09<img\x20loading=\x22lazy\x22\x20decoding=\x22async\x22\x20width=\x221984\x22\x20height=\x22993\x22\x0a\x09\x09\x09\x09\x09\x09\x09\x09src=\x22",
+    "getElementById",
+    "/media/images/body/ch",
+    "260197PvKJrL",
+    "462093faGsJK",
+    "beforeend",
+    "floor",
+    ".jpg",
+    "5490450USFbtI",
+    "random",
+    "7846911IQRgNU",
+  ];
+  _0x5b9e = function () {
+    return _0x500e93;
+  };
+  return _0x5b9e();
+}
